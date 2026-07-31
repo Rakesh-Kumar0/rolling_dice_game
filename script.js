f24334661f7585e925e75f5dd5b909100ecd516f
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-07-31 12:46:57 +0530

    script page

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ document.addEventListener("DOMContentLoaded", function(){
             // Generate a random dice value between 1 and 6
             const diceValue = Math.floor(Math.random()*6)+1;
             // Create the path to the corresponding dice image;
-            const diceImage = './assets/dice-${diceValue}.png';
+            const diceImage = `./assets/dice-${diceValue}.png`;
             // Set the src attribute of the dice image element to display the rolled dice
             diceImagesrc.attributes.src.textContent = diceImage;
 
             // Change the background colors of the players' panels based on the current player
-            if(currentPlayer == 1){
+            if(currentPlayer === 1){
                 player1.style.backgroundColor = "#7a236075";
                 player2.style.backgroundColor = "#581845";
             }
@@ -38,20 +38,20 @@ document.addEventListener("DOMContentLoaded", function(){
                 player2.style.backgroundColor = "#7a236075"
             }
             // Check if the rolled dice value is 6
-            if(diceValue == 6){
+            if(diceValue === 6){
                 // Mark the current player as the winner of the round
                 winner = currentPlayer;
                 // Update the wins array based on the current player
                 winner === 2 ? (wins = [wins[0], wins[1] + 1]) : (wins = [wins[0] + 1, wins[1]]);
                 // Check if either player has won 2 or more rounds and call the checkWinnerFun if true
-                if(wins[0] >=2 || wins[1] >=2) return checkWinnweFun();
+                if(wins[0] >=2 || wins[1] >=2) return checkWinnerFun();
 
                 // If the round is not the last one (less than 3), proceed to the next steps
                 if(round < 3){
                     // Update the points
                     updatePointsFun();
                     // Display a message indicating the winner of the round and round number
-                    winnerMessage.textContent = 'player ${winner} win round ${round} 🔥';
+                    winnerMessage.textContent = `player ${winner} win round ${round} 🔥`;
                     // Display the button to play the next round and hide the roll button.
                     playNextRoundBtn.style.display = "flex";
                     rollBtn.style.display = "none";
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function(){
     // This function updates the points display based on the winner of the current round.
     function updatePointsFun(){
         // Check if the winner is Player 1
-        if(winner == 1){
+        if(winner === 1){
             // Display a checkmark symbol for Player 1 on the current round's points tracker
             playerOnePoints.children[round -1].textContent = "✔️";
             playerTwoPoints.children[round -1].textContent = "❌";
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function(){
             playerTwoPoints.children[round -1].style.backgroundColor = "transparent";
         }
         // Check if the winner is Player 1
-        if(winner == 2){
+        if(winner === 2){
             // Display a checkmark symbol for Player 1 on the current round's points tracker
             playerOnePoints.children[round -1].textContent = "❌";
             playerTwoPoints.children[round -1].textContent = "✔️";
